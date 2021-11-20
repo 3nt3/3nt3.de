5fafc7969700bc3d4af43bb26d55357ec9a895aa
3nt3 <gott@3nt3.de>
2021-11-21 00:33:32 +0100

    make links that are not openable in a browser not clickable
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/Main.elm
+++ src/Main.elm
@@ -22,8 +22,16 @@ sides =
     { top = 0, left = 0, right = 0, bottom = 0 }
 
 
+listItem child =
+    paragraph [] [ el [ Font.family [ Font.typeface "Hack", Font.monospace ] ] <| text "-> ", child ]
+
+
+listLink url label =
+    listItem (link [] { url = url, label = el [ Font.underline, Font.color linkColor ] <| text label })
+
+
 annotatedLink url label description =
-    paragraph [] [ el [ Font.family [ Font.typeface "Hack", Font.monospace ] ] <| text "-> ", link [] { url = url, label = el [ Font.underline, Font.color linkColor ] <| text label }, text " — ", description ]
+    paragraph [] [ listLink url label, text " — ", description ]
 
 
 main : Html msg
@@ -48,19 +56,22 @@ main =
                     ]
                     [ paragraph [ Font.size 50 ] [ el [ Font.bold ] <| text "3nt3.de", text " - a duck's personal homepage" ]
                     , el [] <| text "Here I host some services some of which you - as a stranger - can actually use too!"
-                    , el [ paddingEach { sides | top = 40 }, Font.bold, Font.size 40 ] <| text "My stuff"
+                    , column [ paddingEach { sides | left = 20 }, spacing 10 ]
+                        []
+                    , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "My accounts"
+                    , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "My stuff"
                     , column [ paddingEach { sides | left = 20 }, spacing 10 ]
                         [ annotatedLink "https://hausis.3nt3.de" "hausis.3nt3.de" (text "a homework organizer I wrote in 2020/21 with moodle integration")
                         , annotatedLink "https://schule.3nt3.de" "schule.3nt3.de" (text "all my school documents for copying homework or learning idk")
                         ]
-                    , el [ paddingEach { sides | top = 40 }, Font.bold, Font.size 40 ] <| text "Stuff I self-host"
+                    , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "Stuff I self-host"
                     , column [ paddingEach { sides | left = 20 }, spacing 10 ]
                         [ annotatedLink "https://git.teich.3nt3.de" "git.teich.3nt3.de" (text "a gitea instance")
                         , annotatedLink "https://le.teich.3nt3.de" "le.teich.3nt3.de" (text "le teich")
                         , annotatedLink "https://phpmyadmin.teich.3nt3.de" "phpmyadmin.teich.3nt3.de" (text "phpmyadmin (playground for testing stuff)")
-                        , annotatedLink "teich.3nt3.de:25565" "teich.3nt3.de" (text "a survival mineraft server")
-                        , annotatedLink "mumble://3nt3.de:64738" "3nt3.de:64738" (text "a mumble server")
-                        , annotatedLink "3nt3.de" "3nt3.de" (text "a matrix homeserver")
+                        , listItem (paragraph [] [ text "teich.3nt3.de — a survival minecraft server (1.17.1 spigot)" ])
+                        , annotatedLink "mumble://3nt3.de:64738" "3nt3.de" (text "a mumble server")
+                        , listItem (paragraph [] [ text "3nt3.de  —  a matrix homeserver" ])
                         , annotatedLink "https://mumble.3nt3.de" "mumble.3nt3.de" (text "web control player for botamusique")
                         , annotatedLink "https://tex.3nt3.de" "tex.3nt3.de" (text "an overleaf instance")
                         ]

1a9f61df275d990ecc905e6d41ee5065dc2a386c
3nt3 <gott@3nt3.de>
2023-01-29 21:29:17 +0100

    change le pronouns
    Signed-off-by: 3nt3 <gott@3nt3.de>

--- src/Main.elm
+++ src/Main.elm
@@ -64,7 +64,7 @@ main =
                     , spacing 20
                     ]
                     [ paragraph [ Font.size 50 ] [ el [ Font.bold ] <| text "3nt3.de 🏳️\u{200D}🌈🏳️\u{200D}⚧️", text " — a duck's personal  website" ]
-                    , el [] <| text "Nia, 17/o, she/they"
+                    , el [] <| text "Nia, 17/o, she/her"
                     , el [] <| text "I like to code and hack stuff"
                     , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "My accounts"
                     , column [ paddingEach { sides | left = 20 }, spacing 10 ]
@@ -77,7 +77,7 @@ main =
                     , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "Stuff I created"
                     , column [ paddingEach { sides | left = 20 }, spacing 10 ]
                         [ annotatedLink "https://schule.3nt3.de" "schule.3nt3.de" (text "all my school documents for copying homework or learning idk")
-                        , annotatedLink "https://quotes.3nt3.de" "quotes.3nt3.de" (text "(very ugly) hacked together voting platform for quotes from my friend-group's discord server to rank them")
+                        , annotatedLink "https://quotes.3nt3.de" "quotes.3nt3.de" (text "voting platform for quotes from my friend-group's discord server to rank them")
                         , annotatedLink "https://nachhilfe.gymhaan.de" "nachhilfe.gymhaan.de" (text "I wrote most of it but it isn't hosted by me")
                         ]
                     , el [ paddingEach { sides | top = 20 }, Font.bold, Font.size 40 ] <| text "Stuff I self-host"
@@ -87,7 +87,7 @@ main =
                         , annotatedLink "https://mumble.3nt3.de" "mumble.3nt3.de" (text "web control player for botamusique")
                         ]
                     , el [ centerX, Font.color <| rgb 0.2 0.2 0.2 ] <| paragraph [] [ text "Made by me with elm. ", link [] { url = "https://github.com/3nt3/3nt3.de", label = el [ Font.color <| rgb 0.3 0.3 0.3 ] <| text "source code" } ]
-                    , html <| a [(attribute "rel") "me", Html.Attributes.href "https://chaos.social/@3nt3", style "display" "none"] [Html.text "Mastodon"] 
+                    , html <| a [(attribute "rel") "me", Html.Attributes.href "https://chaos.social/@3nt3", style "display" "none"] [Html.text "Mastodon"]
                     ]
                 )
             )
